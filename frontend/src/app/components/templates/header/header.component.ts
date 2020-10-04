f15f1e2f936d0af0d7ecd4a46cf6fb7b01e49e7a
edson-alexandre <edson.ass@gmail.com>
2020-10-04 14:44:08 -0300

    Comunição central entre componentes através de service

--- frontend/src/app/components/templates/header/header.component.ts
+++ frontend/src/app/components/templates/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
+import { HeaderData } from './header-data-model';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  headerData: HeaderData
 
-  constructor() { }
+  constructor(private headerService: HeaderService) { }
 
   ngOnInit(): void {
+    
+  }
+
+  get title(): string {
+    return this.headerService.headerData.title
+  }
+  
+  get icon(): string {
+    return this.headerService.headerData.icon
+  }
+  
+  get routeUrl(): string {
+    return this.headerService.headerData.routeUrl
   }
 
 }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from '../../components/templates/header/header.service';
+import { HeaderData } from '../../components/templates/header/header-data-model';
 
 @Component({
   selector: 'app-product-crud',
@@ -8,9 +10,18 @@ import { Router } from '@angular/router';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private headerService: HeaderService
+  ) { 
+    this.headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: 'products'
+    }
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
   navigateToProductCreate(): void {
